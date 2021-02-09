9b714a1093fc4b6be0aad2bfc5fb0a11f118624c
fanny <fanny.miech@le-campus-numerique.fr>
2021-02-09 11:28:01 +0100

    add AppEssai

---
+++ WpfAppEssai2/Model/Model1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppEssai2.Model
+{
+    public class Model1
+    {
+    }
+}
